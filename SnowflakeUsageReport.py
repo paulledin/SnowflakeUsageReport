2f748fc594cd373d38fa8fb82ea4600fad2c1ecc
Paul Ledin <paul_ledin@uwalumni.com>
2024-11-12 16:47:53 -0600

    Update SnowflakeUsageReport.py

--- SnowflakeUsageReport.py
+++ SnowflakeUsageReport.py
@@ -17,6 +17,10 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
+st.set_page_config(
+    page_title="America's Credit Unions",
+    layout="wide",
+    initial_sidebar_state="expanded")
 ###############################################################################
 #Function Definitions
 ###############################################################################
@@ -27,12 +31,6 @@ import altair as alt
 #Start building Streamlit App
 ###############################################################################
 thePassPhrase = st.secrets["thePassPhrase"]
-
-st.set_page_config(
-    page_title="America's Credit Unions",
-    layout="wide",
-    initial_sidebar_state="expanded")
-
 with st.sidebar:
     st.markdown('![alt text](https://raw.githubusercontent.com/paulledin/data/master/ACUS.jpg)')
     passphrase = st.text_input("### Please enter the passphrase:")
