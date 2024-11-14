db78da12db4fc1a30c00ee7ff62278ccc8613d48
Paul Ledin <paul_ledin@uwalumni.com>
2024-11-14 16:41:18 -0600

    Update SnowflakeUsageReport.py

--- SnowflakeUsageReport.py
+++ SnowflakeUsageReport.py
@@ -14,13 +14,11 @@ st.set_page_config(
     page_title="America's Credit Unions",
     layout="wide",
     initial_sidebar_state="expanded")
-
-conn = st.connection("snowflake")
 ###############################################################################
 #Function Definitions
 ###############################################################################
 @st.cache_data
-def get_information_schema(databaseName):
+def get_information_schema(dbConn, databaseName):
     session = conn.session()
     retVal = session.sql("SELECT substr(TABLE_NAME, 21, 26) as \"period\" FROM monthly_report.information_schema.tables WHERE table_schema = 'BOTH' and TABLE_NAME like 'AFL_TABLE_1_BYSTATE_%' ").to_pandas()
     
@@ -29,7 +27,7 @@ def get_information_schema(databaseName):
 #Start building Streamlit App
 ###############################################################################
 thePassPhrase = st.secrets["thePassPhrase"]
-
+dbConn = st.connection("snowflake")
 
 with st.sidebar:
     st.markdown('![alt text](https://raw.githubusercontent.com/paulledin/data/master/ACUS.jpg)')
@@ -40,7 +38,7 @@ if (passphrase != thePassPhrase):
         st.markdown('# Passphrase not correct....')
         st.markdown('### Please try again or contact: pledin@americascreditunions.org for assistance.')
 else:
-    report_periods = get_information_schema('ACUS_DATA')
+    report_periods = get_information_schema(dbConn, 'ACUS_DATA')
     st.write(report_periods)
     
      
